0ed69a294e3da496c42ad76c8d541b689666b489
amrmohamedhassan <amr.houria70@gmail.com>
2024-09-23 18:32:54 +0200

    added Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+//Imports
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
+
+//Pages
+import Layout from "./Layout";
+import Home from "./Home";
+import About from "./About";
+
+//Router
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element= {<Home />} />
+      <Route path="/about" element= {<About />} />
+    </Route>
+  )
+)
+
+//Main_Function
 function App() {
   return (
-    <div className="App">
-      <h1>Home</h1>
+    <div className="app">
+    <RouterProvider router={router} />        
     </div>
   );
 }
 
-export default App;
+export default App; 
---
+++ src/Layout.js
@@ -0,0 +1,40 @@
+import { Link, NavLink, Outlet } from 'react-router-dom';
+import React, { useState } from 'react';
+
+const Layout = () => {
+  
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleBurgerClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="layout">
+      <div className="nav">
+        <div className="cont">
+          <div className="burger-cont">
+            <Link to="/"><img src="/Icons/Logo.png" alt="Goldstern" /></Link>
+            <div className={`burger ${isOpen ? 'open' : ''}`} onClick={handleBurgerClick}>
+              <div className="line"></div>
+              <div className="line"></div>
+              <div className="line"></div>
+            </div>
+          </div>
+          <ul className={`list ${isOpen ? 'open' : ''}`}>
+            <NavLink onClick={handleBurgerClick} to="/">HOME</NavLink>
+            <NavLink onClick={handleBurgerClick} to="/about">ABOUT</NavLink>
+          </ul>
+        </div>
+      </div>
+      <main>
+        <Outlet />
+      </main>
+      <div className="footer">
+        <h1>Footer</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
